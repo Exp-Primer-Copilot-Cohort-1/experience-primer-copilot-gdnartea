2a79a97736cb63723ddd163bc4f11d84ae9432aa
gdnartea <166901581+gdnartea@users.noreply.github.com>
2024-04-19 12:29:37 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,17 +1,44 @@
+// create webserver
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const qs = require('querystring');
 
-// create web server
+const comments = [];
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
+  const { pathname } = url.parse(req.url);
+  if (pathname === '/') {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('404 Not Found');
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  } else if (pathname === '/comments' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(comments));
+  } else if (pathname === '/comments' && req.method === 'POST') {
+    let body = '';
+    req.on('data', (data) => {
+      body += data;
+    });
+    req.on('end', () => {
+      const comment = qs.parse(body);
+      comments.push(comment);
+      res.writeHead(201, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(comment));
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('404 Not Found');
+  }
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
 });
-
-
-
-
 
